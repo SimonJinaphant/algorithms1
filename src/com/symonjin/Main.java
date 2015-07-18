49b778ace8a61b7e6dd38b6fdb8dc1a41f633554
Simon Jinaphant <SimonJinaphant@users.noreply.github.com>
2015-07-18 14:32:54 -0700

    Re-organization of code

--- src/com/symonjin/Main.java
+++ src/com/symonjin/Main.java
@@ -1,20 +1,19 @@
 package com.symonjin;
 
-import com.symonjin.algorithms.*;
-import com.symonjin.sorting.*;
+import com.symonjin.sorting.NLogNSort;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Integer[] arr = {50,234,10,-20,10,39,2};
-        for(Integer i : arr){
+        Integer[] arr = {50, 234, 10, -20, 10, 39, 2};
+        for (Integer i : arr) {
             System.out.print(i + ", ");
         }
         System.out.println();
 
         NLogNSort.mergeSort(arr);
-        for(Integer i : arr){
+        for (Integer i : arr) {
             System.out.print(i + ", ");
         }
         System.out.println();
