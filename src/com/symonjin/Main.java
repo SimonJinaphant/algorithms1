0c787f9d7a7f3936af2a5986f6b0c09429331ae4
Simon Jinaphant <SimonJinaphant@users.noreply.github.com>
2015-07-26 23:04:17 -0700

    Added BinaryMinHeap

--- src/com/symonjin/Main.java
+++ src/com/symonjin/Main.java
@@ -1,21 +1,21 @@
 package com.symonjin;
 
-import com.symonjin.collections.BinaryMaxHeap;
+import com.symonjin.collections.BinaryMinHeap;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Integer[] arr = {50, 234, 10, 20, -30, 39, 2};
-        BinaryMaxHeap heap = new BinaryMaxHeap(arr.length);
+        BinaryMinHeap heap = new BinaryMinHeap(arr.length);
 
         for (Integer i : arr) {
             heap.insert(i);
         }
 
         while (!heap.isEmpty()){
-            System.out.println(heap.getMaxElement());
-            heap.removeMax();
+            System.out.println(heap.getMinElement());
+            heap.removeMin();
         }
         
     }
