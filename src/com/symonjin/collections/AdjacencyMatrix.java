88d7db1a53768108a041af5f77438ffa3fa4f1be
Simon Jinaphant <SimonJinaphant@users.noreply.github.com>
2015-11-07 22:05:47 -0800

    Added base for Adjacency Matrix

---
+++ src/com/symonjin/collections/AdjacencyMatrix.java
@@ -0,0 +1,23 @@
+package com.symonjin.collections;
+
+import java.util.List;
+
+/**
+ * Created by Simon on 2015-11-07.
+ */
+public class AdjacencyMatrix implements Graph {
+
+
+    public void addVertex(Vertex v){
+
+    }
+
+    public void addEdge(Vertex a, Vertex b){
+
+    }
+
+    public List<Vertex> getVertices(){
+        return null;
+    }
+
+}
