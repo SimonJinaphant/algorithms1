7943ba13c721099c8c315ddbc64bddd5e0b1addc
Simon Jinaphant <SimonJinaphant@users.noreply.github.com>
2015-11-08 22:29:34 -0800

    Updated base for adj matrix

--- src/com/symonjin/collections/AdjacencyMatrix.java
+++ src/com/symonjin/collections/AdjacencyMatrix.java
@@ -1,22 +1,34 @@
 package com.symonjin.collections;
 
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by Simon on 2015-11-07.
  */
 public class AdjacencyMatrix implements Graph {
 
-    public void addVertex(Vertex v){
+    private ArrayList<Vertex> ordering = new ArrayList<>();
+    private ArrayList<BitSet> matrix;
 
+    public void addVertex(Vertex v){
+        ordering.add(v);
+        for(BitSet row : matrix){
+            row.set(ordering.size(), false);
+        }
+        matrix.add(new BitSet(ordering.size()));
     }
 
     public void addEdge(Vertex a, Vertex b){
+        int aPosition = ordering.indexOf(a);
+        int bPosition = ordering.indexOf(b);
 
+        matrix.get(aPosition).set(bPosition, true);
     }
 
     public Set<Vertex> getVertices(){
-        return null;
+        HashSet<Vertex> vertices = new HashSet<>();
+        vertices.addAll(ordering);
+        return vertices;
     }
 
 }
